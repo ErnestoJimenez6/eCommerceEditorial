cac1f10470ee8b5756ee0bc27e446ab603402574
Ernesto <zynhop@gmail.com>
2024-08-03 20:56:02 -0300

    NavBarLinkNuevo

---
+++ src/components/Navbar/NavBarLink.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/prop-types */
+export const NavBarLink=({genre})=>{
+    return(
+        <li className='nav-item'>
+            <a className='nav-link' aria-current='page' href='#'>{genre}</a>
+        </li>
+    )
+}
+
+export default NavBarLink
